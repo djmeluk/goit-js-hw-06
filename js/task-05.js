6534eb8da6e2e01d32d0fe4f6c6d1d6bfd92e335
Oleh Tomashevskiy <kulemjd@ukr.net>
2022-12-16 22:21:10 +0200

    task 06 completed

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 
 const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
-inputEl.addEventListener('input',onInputChange,);
+inputEl.addEventListener('input',onInputChange);
 const oldTextSpanEl = spanEl.textContent;
 function onInputChange(event) { 
     spanEl.textContent = event.currentTarget.value;
