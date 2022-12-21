2ed05b2ee43ce0c918ff887263e98ae2e348342f
Oleh Tomashevskiy <kulemjd@ukr.net>
2022-12-21 16:57:49 +0200

    task 7,8,9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector("body")
+const spanEl = document.querySelector(".color")
+const buttonChangeColor = document.querySelector(".change-color")
+
+
+buttonChangeColor.addEventListener("click",bodyChangeColor)
+function bodyChangeColor() {
+ const color = bodyEl.style.backgroundColor = getRandomHexColor()
+  spanEl.textContent = color
 }
